b21021b0b39bff64123b8b8402d9388aa7c14255
Gahramanova Laman <lemangahramanoval@gmail.com>
2025-02-22 21:32:20 +0400

    some config files

---
+++ fuse-react-2/src/app/configs/navigationConfig.js
@@ -0,0 +1,29 @@
+import i18next from 'i18next';
+import ar from './navigation-i18n/ar';
+import en from './navigation-i18n/en';
+import tr from './navigation-i18n/tr';
+
+i18next.addResourceBundle('en', 'navigation', en);
+i18next.addResourceBundle('tr', 'navigation', tr);
+i18next.addResourceBundle('ar', 'navigation', ar);
+
+const navigationConfig = [
+  {
+    id: 'example-component',
+    title: 'Example',
+    translate: 'EXAMPLE',
+    type: 'item',
+    icon: 'heroicons-outline:star',
+    url: 'example',
+  },
+  {
+    id: 'about-component', // New item for about page
+    title: 'About',
+    translate: 'ABOUT', // Make sure to add this in the translation files
+    type: 'item',
+    icon: 'heroicons-outline:information-circle', // Appropriate icon for the About section
+    url: '/example/about', // New URL pointing to about page
+  },
+];
+
+export default navigationConfig;
