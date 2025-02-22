d11b07900ceab98a682fc363a925daf216822cf3
Gahramanova Laman <lemangahramanoval@gmail.com>
2025-02-22 21:35:04 +0400

    example, about and not found pages and their configuration

---
+++ fuse-react-2/src/app/main/about/About.js
@@ -0,0 +1,40 @@
+/* eslint-disable prettier/prettier */
+import { styled } from "@mui/material/styles";
+import { useTranslation } from "react-i18next";
+import FusePageSimple from "@fuse/core/FusePageSimple";
+import AdminPageStructure from "@fuse/core/About/AdminPageStructure";
+
+const Root = styled(FusePageSimple)(({ theme }) => ({
+    "& .FusePageSimple-header": {
+        backgroundColor: theme.palette.background.paper,
+        borderBottomWidth: 1,
+        borderStyle: "solid",
+        borderColor: theme.palette.divider,
+    },
+    '& .FusePageSimple-toolbar': {},
+    "& .FusePageSimple-content": {},
+    "& .FusePageSimple-sidebarHeader": {},
+    "& .FusePageSimple-sidebarContent": {},
+}));
+
+function AboutPage(props) {
+    const { t } = useTranslation("aboutPage");
+
+    return (
+        <Root
+            header={
+                <div className="p-24">
+                    <h4>{t("TITLE")}</h4>
+                </div>
+            }
+            content={
+                <div className="p-24">
+                    <AdminPageStructure/>
+                </div>
+            }
+            scroll="content"
+        />
+    );
+}
+
+export default AboutPage;
---
+++ fuse-react-2/src/app/main/about/AboutConfig.js
@@ -0,0 +1,26 @@
+import i18next from 'i18next';
+
+import en from './i18n/en';
+import tr from './i18n/tr';
+import ar from './i18n/ar';
+import AboutPage from './About';
+
+i18next.addResourceBundle('en', 'aboutPage', en);
+i18next.addResourceBundle('tr', 'aboutPage', tr);
+i18next.addResourceBundle('ar', 'aboutPage', ar);
+
+const AboutConfig = {
+  settings: {
+    layout: {
+      config: {},
+    },
+  },
+  routes: [
+    {
+      path: '/example/about',
+      element: <AboutPage />,
+    },
+  ],
+};
+
+export default AboutConfig;
