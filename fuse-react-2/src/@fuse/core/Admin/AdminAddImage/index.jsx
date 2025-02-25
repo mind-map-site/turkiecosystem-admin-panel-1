eb5b5a8988cf720482e91b3549568094fa02365c
Gahramanova Laman <lemangahramanoval@gmail.com>
2025-02-25 14:41:16 +0400

    more general usable for admin add image section

---
+++ fuse-react-2/src/@fuse/core/Admin/AdminAddImage/index.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import { Button, Card, CardContent, CardHeader, CardActions, Typography, TextField } from '@mui/material';
+import { sendAboutImage } from 'src/@mock-api/api/about-api';
+import { toast } from 'react-toastify';
+
+const AdminAddImage = ({ id, setReload, image, addImage }) => {
+  const [file, setFile] = useState(null);
+  const [value, setValue] = useState(null);
+
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files ? e.target.files[0] : null;
+    setFile(selectedFile);
+  };
+
+  const handleChange = (e) => {
+    setValue(e.target.value)
+  };
+
+  const handleClickSubmit = async (e) => {
+    e.preventDefault();
+    if (file) {
+      const formData = new FormData();
+      formData.append('image', file);
+      try {
+        const res = await addImage(id || value, formData);
+        if (res.status === 201) {
+          toast.success('Image uploaded successfully');
+          setReload((prev) => prev + 1);
+        } else {
+          toast.error('Failed to upload image');
+        }
+      } catch {
+        toast.error('Failed to upload image. There was a problem with your request.');
+      }
+    }
+  };
+
+  return (
+    <form>
+      <Card className="space-y-4">
+        <CardHeader>
+          <Typography variant="h6">Picture</Typography>
+
+          <Typography variant="body2" color="textSecondary">
+            You can upload your image for about tab.
+          </Typography>
+        </CardHeader>
+        <CardContent className="space-y-2">
+          {
+            !id &&
+            <>
+              <Typography variant="body2">Enter Image Id</Typography>
+              <TextField
+                onChange={handleChange}
+                value={value}
+                id='image_id'
+                name='image_id'
+              />
+            </>
+          }
+
+          <Typography variant="body2" className='mt-2'>New Picture</Typography>
+          <input
+            id="picture"
+            type="file"
+            onChange={handleFileChange}
+            className="w-full p-2 border rounded-md"
+          />
+        </CardContent>
+        <CardContent className="space-y-2">
+          <Typography variant="body2">Your Current Image</Typography>
+          {image ? (
+            <img src={image.href.url} alt="about" />
+          ) : (
+            <Typography variant="body2">There is no image</Typography>
+          )}
+        </CardContent>
+        <CardActions>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleClickSubmit}
+            className="w-full"
+          >
+            Submit
+          </Button>
+        </CardActions>
+      </Card>
+    </form>
+  );
+};
+
+export default AdminAddImage;
