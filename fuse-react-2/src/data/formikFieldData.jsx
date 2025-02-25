994810d63921c99b27889840498b344fa57229fa
Gahramanova Laman <lemangahramanoval@gmail.com>
2025-02-25 14:53:06 +0400

    update formik field data

--- fuse-react-2/src/data/formikFieldData.jsx
+++ fuse-react-2/src/data/formikFieldData.jsx
@@ -20,3 +20,27 @@ export const useAboutContentValidation = () => {
       .required('Content is required'),
   });
 };
+
+
+export const useNewsFormValidation = () => {
+  return object({
+    titleEn: string()
+      .min(10, 'Text must be at least 10 characters long.')
+      .required('Title is required'),
+    titleRu: string()
+      .min(10, 'Text must be at least 10 characters long.')
+      .required('Title is required'),
+    titleAz: string()
+      .min(10, 'Text must be at least 10 characters long.')
+      .required('Title is required'),
+    descriptionEn: string()
+      .min(10, 'Text must be at least 10 characters long.')
+      .required('Description is required'),
+    descriptionRu: string()
+      .min(10, 'Text must be at least 10 characters long.')
+      .required('Description is required'),
+    descriptionAz: string()
+      .min(10, 'Text must be at least 10 characters long.')
+      .required('Description is required'),
+  })
+}
