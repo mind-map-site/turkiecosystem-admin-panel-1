c973029f09780fe4d1d4608eb7e7c53805d2bd15
Gahramanova Laman <lemangahramanoval@gmail.com>
2025-02-25 16:56:25 +0400

    add props to sections of news

--- fuse-react-2/src/app/main/news/News.js
+++ fuse-react-2/src/app/main/news/News.js
@@ -3,7 +3,8 @@ import { styled } from "@mui/material/styles";
 import { useTranslation } from "react-i18next";
 import FusePageSimple from "@fuse/core/FusePageSimple";
 import AdminPageStructure from "@fuse/core/Admin/AdminPageStructure";
-import { deleteNewsImage, getNewsContent, getSingleNewsById, sendNewsImage, updateNewsContent } from "src/@mock-api/api/news-api";
+import { deleteNewsContent, deleteNewsImage, getNewsContent, getSingleNewsById, sendNewsContent, sendNewsImage, updateNewsContent } from "src/@mock-api/api/news-api";
+import { newsCreateFormInputs, newsFormInitialValues, useNewsFormValidation } from "src/data/formikFieldData";
 
 const Root = styled(FusePageSimple)(({ theme }) => ({
     "& .FusePageSimple-header": {
@@ -34,9 +35,9 @@ function NewsPage(props) {
                         [
                             { label: "Show News", value: "ShowNews", props:{getAllData:getNewsContent } },
                             { label: "Show Single News", value: "ShowSingleNews", props: { getSingleData: getSingleNewsById } },
-                            { label: "Update News", value: "UpdateNews", props:{updateData, updateNewsContent} },
-                            { label: "Create News", value: "CreateNews" },
-                            { label: "Delete News", value: "DeleteNews" },
+                            { label: "Update News", value: "UpdateNews", props:{updateData: updateNewsContent} },
+                            { label: "Create News", value: "CreateNews", props: { createData: sendNewsContent, inputs: newsCreateFormInputs, useValidation: useNewsFormValidation, initialValues: newsFormInitialValues, section:"news" } },
+                            { label: "Delete News", value: "DeleteNews", props:{deleteData: deleteNewsContent}},
                             { label: "Add Image", value: "AddImg", props:{ addImage:sendNewsImage } },
                             { label: "Delete Image", value: "DeleteImg", props: {deleteImage:deleteNewsImage} }
                         ]
