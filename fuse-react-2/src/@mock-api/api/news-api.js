e798dcbf432da5648cc4c49ec9655e847d241027
Gahramanova Laman <lemangahramanoval@gmail.com>
2025-02-25 14:43:30 +0400

    all api calls for news are added

---
+++ fuse-react-2/src/@mock-api/api/news-api.js
@@ -0,0 +1,45 @@
+import axiosInstance from "./axiosInstance";
+
+export const getNewsContent = async () => {
+    const { data } = await axiosInstance.get("/news");
+    return data;
+}
+
+export const getSingleNewsById = async (id) => {
+    const { data } = await axiosInstance.get(`/news/${id}`);
+    return data;
+}
+
+export async function sendNewsContent(data) {
+    const res = await axiosInstance.post("/news", data);
+    return res;
+}
+
+export async function updateNewsContent(id, data) {
+    const res = await axiosInstance.put(`/news/${id}`, data, {
+        headers: {
+            "Content-Type": "multipart/form-data",
+        }
+    });
+    return res;
+}
+
+export async function sendNewsImage(id, data) {
+    const res = await axiosInstance.post(`/news/${id}/image`, data, {
+        headers: {
+            "Content-Type": "multipart/form-data",
+        }
+    });
+    return res;
+}
+
+export async function deleteNewsImage(id, publicId) {
+    console.log(id, publicId)
+    const res = await axiosInstance.delete(`/news/${id}/image/${publicId}`);
+    return res;
+}
+
+export async function deleteNewsContent(id, publicId) {
+    const res = await axiosInstance.delete(`/news/${id}`);
+    return res;
+}
