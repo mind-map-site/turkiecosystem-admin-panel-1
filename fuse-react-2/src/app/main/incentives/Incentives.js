f06fb587eb787a36708e55f39cca80f8481eb685
Gahramanova Laman <lemangahramanoval@gmail.com>
2025-03-19 08:05:00 +0400

    writing all tabs of incentives

---
+++ fuse-react-2/src/app/main/incentives/Incentives.js
@@ -0,0 +1,53 @@
+/* eslint-disable prettier/prettier */
+import { styled } from "@mui/material/styles";
+import { useTranslation } from "react-i18next";
+import FusePageSimple from "@fuse/core/FusePageSimple";
+import { deleteIncentiveContent, deleteIncentiveImage, getIncentiveContent, getSingleIncentiveById, sendIncentiveContent, sendIncentiveImage, updateIncentiveContent } from "src/@mock-api/api/incentives-api";
+import AdminPageStructure from "@fuse/core/Admin/AdminPageStructure";
+import { newsCreateFormInputs, newsFormInitialValues, useNewsFormValidation } from "src/data/formikFieldData";
+
+
+const Root = styled(FusePageSimple)(({ theme }) => ({
+    "& .FusePageSimple-header": {
+        backgroundColor: theme.palette.background.paper,
+        borderBottomWidth: 1,
+        borderStyle: "solid",
+        borderColor: theme.palette.divider,
+    },
+    '& .FusePageSimple-toolbar': {},
+    "& .FusePageSimple-content": {},
+    "& .FusePageSimple-sidebarHeader": {},
+    "& .FusePageSimple-sidebarContent": {},
+}));
+
+function IncentivesPage(props) {
+    const { t } = useTranslation("incentivesPage");
+
+    return (
+        <Root
+            header={
+                <div className="p-24">
+                    <h4>{t("INCENTIVES")}</h4>
+                </div>
+            }
+            content={
+                <div className="p-24">
+                    <AdminPageStructure getContentAPI={getIncentiveContent} description={"Choose an action to perform on the Incentives Page."} actions={
+                        [
+                            { label: "Show Incentives", value: "ShowNews", props:{getAllData:getIncentiveContent } },
+                            { label: "Show Single Incentive", value: "ShowSingleNews", props: { getSingleData: getSingleIncentiveById } },
+                            { label: "Update Incentive", value: "UpdateNews", props: { updateData: updateIncentiveContent, inputs: newsCreateFormInputs, useValidation: useNewsFormValidation, initialValues: newsFormInitialValues, section: "incentive"} },
+                            { label: "Create Incentive", value: "CreateNews", props: { createData: sendIncentiveContent, inputs: newsCreateFormInputs, useValidation: useNewsFormValidation, initialValues: newsFormInitialValues, section:"incentive" } },
+                            { label: "Delete Incentive", value: "DeleteNews", props:{deleteData: deleteIncentiveContent}},
+                            { label: "Add Image", value: "AddImg", props:{ addImage: sendIncentiveImage, section:"incentive" } },
+                            { label: "Delete Image", value: "DeleteImg", props: {deleteImage:deleteIncentiveImage} }
+                        ]
+                    } />
+                </div>
+            }
+            scroll="content"
+        />
+    );
+}
+
+export default IncentivesPage;
