992e0fc6ba4d22c7100a49b8c4ce86fba6e5c298
Gahramanova Laman <lemangahramanoval@gmail.com>
2025-03-19 08:02:48 +0400

    writing all requests for api endpoints of incentives

---
+++ fuse-react-2/src/@mock-api/api/incentives-api.js
@@ -0,0 +1,40 @@
+import axiosInstance from "./axiosInstance";
+
+export const getIncentiveContent = async () => {
+    const { data } = await axiosInstance.get("/incentive");
+    return data;
+}
+
+export const getSingleIncentiveById = async (id) => {
+    const { data } = await axiosInstance.get(`/incentive/${id}`);
+    return data;
+}
+
+export async function sendIncentiveContent(data) {
+    const res = await axiosInstance.post("/incentive", data);
+    return res;
+}
+
+export async function updateIncentiveContent(id, data) {
+    const res = await axiosInstance.put(`/incentive/${id}`, data, );
+    return res;
+}
+
+export async function sendIncentiveImage(id, data) {
+    const res = await axiosInstance.post(`/incentive/${id}/image`, data, {
+        headers: {
+            "Content-Type": "multipart/form-data",
+        }
+    });
+    return res;
+}
+
+export async function deleteIncentiveImage(id, publicId) {
+    const res = await axiosInstance.delete(`/incentive/${id}/image/${publicId}`);
+    return res;
+}
+
+export async function deleteIncentiveContent(id) {
+    const res = await axiosInstance.delete(`/incentive/${id}`);
+    return res;
+}
