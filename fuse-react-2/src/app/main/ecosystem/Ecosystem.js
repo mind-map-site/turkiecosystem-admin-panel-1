6ebd7f38c3cce3feee8a2f21bbc513893c255a4c
Gahramanova Laman <lemangahramanoval@gmail.com>
2025-04-02 00:48:04 +0400

    adding root  page of ecosystem for admin page structure

---
+++ fuse-react-2/src/app/main/ecosystem/Ecosystem.js
@@ -0,0 +1,54 @@
+/* eslint-disable prettier/prettier */
+import { styled } from "@mui/material/styles";
+import { useTranslation } from "react-i18next";
+import FusePageSimple from "@fuse/core/FusePageSimple";
+import AdminPageStructure from "@fuse/core/Admin/AdminPageStructure";
+import { deleteEcosystemContent, deleteEcosystemImage, getEcosystemContent, getSingleEcosystemById, sendEcosystemContent, sendEcosystemImage, updateEcosystemContent } from "src/@mock-api/api/ecosystem-api";
+import { ecosystemCreateFormInputs, ecosystemFormInitialValues, newsCreateFormInputs, useEcosystemFormValidation } from "src/data/formikFieldData";
+
+const Root = styled(FusePageSimple)(({ theme }) => ({
+    "& .FusePageSimple-header": {
+        backgroundColor: theme.palette.background.paper,
+        borderBottomWidth: 1,
+        borderStyle: "solid",
+        borderColor: theme.palette.divider,
+    },
+    '& .FusePageSimple-toolbar': {},
+    "& .FusePageSimple-content": {},
+    "& .FusePageSimple-sidebarHeader": {},
+    "& .FusePageSimple-sidebarContent": {},
+}));
+
+function AboutPage(props) {
+    const { t } = useTranslation("ecosystemPage");
+
+    return (
+        <Root
+            header={
+                <div className="p-24">
+                    <h4>{t("TITLE")}</h4>
+                </div>
+            }
+            content={
+                <div className="p-24">
+                    <AdminPageStructure getContentAPI={getEcosystemContent} description={"Choose an action to perform on the Ecosystems Page."} actions={
+                        [
+                            { label: "Show Ecosystems", value: "ShowNews", props: { getAllData: getEcosystemContent, section: "ecosystem" } },
+                            { label: "Show Single Ecosystem", value: "ShowSingleNews", props: { getSingleData: getSingleEcosystemById, section:"ecosystem" } },
+                            { label: "Update Ecosystem", value: "UpdateNews", props: { updateData: updateEcosystemContent, inputs: ecosystemCreateFormInputs, useValidation: useEcosystemFormValidation, initialValues: ecosystemFormInitialValues, section: "ecosystem" } },
+                            { label: "Create Ecosystem", value: "CreateNews", props: { createData: sendEcosystemContent, inputs: ecosystemCreateFormInputs, useValidation: useEcosystemFormValidation, initialValues: ecosystemFormInitialValues, section: "ecosystem" } },
+                            { label: "Delete Ecosystem", value: "DeleteNews", props: { deleteData: deleteEcosystemContent } },
+                            { label: "Add Image", value: "AddImg", props: { addImage: sendEcosystemImage, section: "ecosystem" } },
+                            { label: "Delete Image", value: "DeleteImg", props: { deleteImage: deleteEcosystemImage } },
+                            { label: "Ecosystem Filter Data", value: "EcosystemFilterCrud" },
+                            { label: "Ecosystem Filter Search", value: "EcosystemFilter" }
+                        ]
+                    } />
+                </div>
+            }
+            scroll="content"
+        />
+    );
+}
+
+export default AboutPage;
