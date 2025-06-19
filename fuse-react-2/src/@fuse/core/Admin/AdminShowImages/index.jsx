5cb52ca84124f241f03c78aa4f9cfc3db7fb4542
Gahramanova Laman <lemangahramanoval@gmail.com>
2025-06-19 18:13:16 +0400

    add new section to the admin for showing images

---
+++ fuse-react-2/src/@fuse/core/Admin/AdminShowImages/index.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  TableContainer,
+  Paper,
+  Avatar,
+  Box,
+  Typography
+} from '@mui/material';
+
+const AdminShowImages = ({data, section, setReload}) => {
+    console.log(data, section);
+  return (
+ <TableContainer component={Paper} elevation={2}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>Time</TableCell>
+            <TableCell>Images</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((item) => (
+            <TableRow key={item.id} hover>
+              <TableCell>{item.id}</TableCell>
+              <TableCell>{item.time}</TableCell>
+              <TableCell>
+                <Box display="flex" flexWrap="wrap" gap={1}>
+                  {item.images.map((img, idx) => (
+                    <Box key={idx} display="flex" alignItems="center" gap={1}>
+                      <Avatar
+                        variant="square"
+                        src={img.href.url}
+                        alt={img.href.title || `image-${idx}`}
+                        sx={{ width: 56, height: 56 }}
+                      />
+                      <Box>
+                        <Typography variant="body2">{img.href.title}</Typography>
+                        <Typography variant="caption">PublicId: {img.href.publicId}</Typography>
+                      </Box>
+                    </Box>
+                  ))}
+                </Box>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>  )
+}
+
+export default AdminShowImages
