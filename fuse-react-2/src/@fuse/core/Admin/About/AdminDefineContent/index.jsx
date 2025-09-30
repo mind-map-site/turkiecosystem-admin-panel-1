5affadbad0cec2c286b53cfdbf74ab91d1c5a8e0
Leman Gahramanova <lemangahramanoval@gmail.com>
2025-09-30 12:49:12 +0400

    add contactlik to payload

--- fuse-react-2/src/@fuse/core/Admin/About/AdminDefineContent/index.jsx
+++ fuse-react-2/src/@fuse/core/Admin/About/AdminDefineContent/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   Card,
   CardContent,
@@ -10,30 +10,37 @@ import {
   AccordionDetails,
   TextField,
   Button,
-} from '@mui/material';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { useAboutContentValidation } from 'src/data/formikFieldData';
-import useFormikForm from 'src/hooks/use-formik-form';
-import { sendAboutContent } from 'src/@mock-api/api/about-api';
-import { toast } from 'react-toastify';
-import FormikField from '@fuse/core/FormikField';
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { useAboutContentValidation } from "src/data/formikFieldData";
+import useFormikForm from "src/hooks/use-formik-form";
+import { sendAboutContent } from "src/@mock-api/api/about-api";
+import { toast } from "react-toastify";
+import FormikField from "@fuse/core/FormikField";
 
 const AdminDefineContent = ({ data, setReload }) => {
   const aboutContentValidation = useAboutContentValidation();
 
+
   const aboutContentInitialValues = {
-    contentEn: data.en,
+    contentEn: data.content.en,
     contentRu: data.ru,
     contentAz: data.az,
+    contactLink: data.contactLink,
   };
 
-  const formik = useFormikForm(aboutContentInitialValues, aboutContentValidation, onSubmit);
+  const formik = useFormikForm(
+    aboutContentInitialValues,
+    aboutContentValidation,
+    onSubmit
+  );
 
-  function onSubmit({ contentEn, contentAz, contentRu }) {
+  function onSubmit({ contentEn, contentAz, contentRu, contactLink }) {
     const values = {
-      'content.en': contentEn,
-      'content.ru': contentRu,
-      'content.az': contentAz,
+      "content.en": contentEn,
+      "content.ru": contentRu,
+      "content.az": contentAz,
+      contactLink,
     };
 
     sendAboutContent(values)
@@ -41,7 +48,7 @@ const AdminDefineContent = ({ data, setReload }) => {
         setReload((prev) => prev + 1);
 
         if (res.data.success && res.status === 200) {
-          toast.success('Content has been updated successfully', {
+          toast.success("Content has been updated successfully", {
             position: "top-right",
             autoClose: 5000,
             hideProgressBar: false,
@@ -52,26 +59,37 @@ const AdminDefineContent = ({ data, setReload }) => {
             theme: "light",
           });
         } else {
-          toast.error('Something went wrong');
+          toast.error("Something went wrong");
         }
       })
       .catch((err) => {
-        toast.error('There was a problem with your request: ');
+        toast.error("There was a problem with your request: ");
       });
   }
 
   return (
     <form onSubmit={formik.handleSubmit} className="space-y-4">
-      <Card>
+      <Card className="px-4">
+        <h2 style={{ fontWeight: 600, marginTop: "1rem" }}>Contact Link</h2>
+        <FormikField
+          fullWidth
+          formik={formik}
+          id="contactLink"
+          name="contactLink"
+          placeholder="Add contact Link"
+        />
+        <h2 style={{ fontWeight: 600, marginTop: "1rem" }}>About Content</h2>
         <CardHeader>
           <Typography variant="h6">Content</Typography>
           <Typography variant="body2" color="text.secondary">
-            You can update or create content in three different languages (EN, AZ, RU).
+            You can update or create content
           </Typography>
         </CardHeader>
-        <CardContent className="space-y-4">
+        <CardContent className="space-y-1">
           <Accordion>
-            <AccordionSummary expandIcon={<ExpandMoreIcon />}>English</AccordionSummary>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              English
+            </AccordionSummary>
             <AccordionDetails>
               <FormikField
                 fullWidth
@@ -84,7 +102,7 @@ const AdminDefineContent = ({ data, setReload }) => {
               />
             </AccordionDetails>
           </Accordion>
-
+          {/* 
           <Accordion>
             <AccordionSummary expandIcon={<ExpandMoreIcon />}>Russian</AccordionSummary>
             <AccordionDetails>
@@ -113,7 +131,7 @@ const AdminDefineContent = ({ data, setReload }) => {
                 placeholder="Add Content in Azerbaijani"
               />
             </AccordionDetails>
-          </Accordion>
+          </Accordion> */}
         </CardContent>
         <CardActions className="p-4">
           <Button type="submit" variant="contained" color="primary">
