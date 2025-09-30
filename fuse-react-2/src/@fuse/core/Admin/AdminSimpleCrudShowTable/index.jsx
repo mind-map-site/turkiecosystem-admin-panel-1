e9f59bbf13e8fdc1942aeb98423bc44bcb2c5122
Leman Gahramanova <lemangahramanoval@gmail.com>
2025-09-30 12:50:46 +0400

    correct updateAPI logic for 2 cases

--- fuse-react-2/src/@fuse/core/Admin/AdminSimpleCrudShowTable/index.jsx
+++ fuse-react-2/src/@fuse/core/Admin/AdminSimpleCrudShowTable/index.jsx
@@ -1,58 +1,93 @@
-import handleResponseSuccess from '@fuse/utils/response';
-import { Button } from '@mui/material';
-import CreateSimpleModal from 'app/shared-components/CreateSimpleModal';
-import CrudTable from 'app/shared-components/CrudTable';
-import UpdateSimpleModal from 'app/shared-components/UpdateSimpleModal';
-import React, { useState } from 'react';
+import handleResponseSuccess from "@fuse/utils/response";
+import { Button } from "@mui/material";
+import CreateSimpleModal from "app/shared-components/CreateSimpleModal";
+import CrudTable from "app/shared-components/CrudTable";
+import UpdateSimpleModal from "app/shared-components/UpdateSimpleModal";
+import React, { useState } from "react";
 
-const AdminSimpleCrudShowTable = ({ data, setReload, section, singleGetApi, createApi, deleteApi, updateApi, useDataForm, isFullCrud = true }) => {
-      const { inputs } = useDataForm();
-      
-    // create modal
-    const [openC, setOpenC] = useState(false);
+const AdminSimpleCrudShowTable = ({
+  data,
+  setReload,
+  section,
+  singleGetApi,
+  createApi,
+  deleteApi,
+  updateApi,
+  useDataForm,
+  isFullCrud = true,
+}) => {
+  const { inputs } = useDataForm();
 
-    // update modal 
-    const [updateData, setUpdateData] = useState(null); // for not full cruds
-    const [updateId, setUpdateId] = useState(null);
-    const [openU, setOpenU] = useState(false);
+  // create modal
+  const [openC, setOpenC] = useState(false);
 
-    const handleUpdate = async (id, data_) => {
-        const res = await updateApi(id, data_);
-        handleResponseSuccess(res, "update", setReload)
-    };
+  // update modal
+  const [updateData, setUpdateData] = useState(null); // for not full cruds
+  const [updateId, setUpdateId] = useState(null);
+  const [openU, setOpenU] = useState(false);
 
-    const handleDelete = async (id) => {
-        const res = await deleteApi(id, data);
-        handleResponseSuccess(res, "delete", setReload)
-    };
-
-    const handleCreate = async (data_) => {
-        const res = await createApi(data_);
-        handleResponseSuccess(res, "create", setReload)
+  const handleUpdate = async (id, data_) => {
+    let res;
+    if (id) {
+      res = await updateApi(id, data_);
+    } else {
+      res = await updateApi(data_);
     }
 
+    handleResponseSuccess(res, "update", setReload);
+  };
 
-    return (
-        <div>
-            <Button variant="outlined" onClick={() => setOpenC(true)}>Create {section}</Button>
+  const handleDelete = async (id) => {
+    const res = await deleteApi(id, data);
+    handleResponseSuccess(res, "delete", setReload);
+  };
 
-            <CrudTable
-                title={`${section} Data`}
-                data={data}
-                type=""
-                setReload={setReload}
-                setUpdateId={setUpdateId}
-                 setUpdateData={ setUpdateData}
-                onDelete={handleDelete}
-                setOpenU={setOpenU}
-                isFullCrud={isFullCrud}
-                inputs={inputs}
-            />
-            {/* create simple modal logic  */}
-            {openC && <CreateSimpleModal open={openC} setOpen={setOpenC} handleCreate={handleCreate} useDataForm={useDataForm} />}
-            {openU && <UpdateSimpleModal open={openU} setOpen={setOpenU} updateId={updateId} updateData={updateData} handleUpdate={handleUpdate} useDataForm={useDataForm} singleGetApi={singleGetApi} isFullCrud={isFullCrud} />}
-        </div>
-    );
+  const handleCreate = async (data_) => {
+    const res = await createApi(data_);
+    handleResponseSuccess(res, "create", setReload);
+  };
+
+  return (
+    <div>
+      <Button variant="outlined" onClick={() => setOpenC(true)}>
+        Create {section}
+      </Button>
+
+      <CrudTable
+        title={`${section} Data`}
+        data={data}
+        type=""
+        setReload={setReload}
+        setUpdateId={setUpdateId}
+        setUpdateData={setUpdateData}
+        onDelete={handleDelete}
+        setOpenU={setOpenU}
+        isFullCrud={isFullCrud}
+        inputs={inputs}
+      />
+      {/* create simple modal logic  */}
+      {openC && (
+        <CreateSimpleModal
+          open={openC}
+          setOpen={setOpenC}
+          handleCreate={handleCreate}
+          useDataForm={useDataForm}
+        />
+      )}
+      {openU && (
+        <UpdateSimpleModal
+          open={openU}
+          setOpen={setOpenU}
+          updateId={updateId}
+          updateData={updateData}
+          handleUpdate={handleUpdate}
+          useDataForm={useDataForm}
+          singleGetApi={singleGetApi}
+          isFullCrud={isFullCrud}
+        />
+      )}
+    </div>
+  );
 };
 
 export default AdminSimpleCrudShowTable;
